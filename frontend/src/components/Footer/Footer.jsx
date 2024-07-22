566988990632b3a4a12e715124c47b21d13394a9
Islam Soliman <islam.salama.ie@gmail.com>
2024-07-22 13:18:49 +0300

    folder restructure

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className='footer' id='footer'>
             <div className="footer-content">
                 <div className="footer-content-left">
-                    <img src={assets.logo} alt="" />
+                    <img className='logo' src={assets.logo} alt="" />
                     <p>description text</p>
                     <div className="footer-social-icons">
                         <img src={assets.facebook_icon} alt="" />
@@ -34,7 +34,7 @@ const Footer = () => {
 
             </div>
             <hr />
-            <p className="footer-copyright">Copright 2024 © Tomato.com - All Right Reserved</p>
+            <p className="footer-copyright">Copright 2024 © dishdrift.com - All Right Reserved</p>
         </div>
     )
 }
